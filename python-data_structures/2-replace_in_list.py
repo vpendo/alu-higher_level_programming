4564eca1905b209ac085897d401334d276bd8d5b
vpendo <v.pendo@alustudent.com>
2023-11-06 13:10:30 -0800

    my data

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,6 +1,7 @@
-ce_in_list(my_list, idx, element):
-        if idx >= 0 and idx < len(my_list):
-            my_list[idx] = element
-            return my_list
-        else:
-            return my_list
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx >= 0 and idx < len(my_list):
+        my_list[idx] = element
+        return my_list
+    else:
+        return my_list
