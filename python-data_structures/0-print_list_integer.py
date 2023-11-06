4f7a10524bfea9e2d3c59a419cbe0c0db94487bd
vpendo <v.pendo@alustudent.com>
2023-11-06 02:43:05 -0800

    my data structure

---
+++ python-data_structures/0-print_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    for num in my_list:
+        print("{:d}".format(num))
+# my list
+my_list = [1, 2, 3, 4, 5]
+print_list_integer(my_list)
