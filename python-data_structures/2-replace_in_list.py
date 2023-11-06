8fc055fd3a506fabe4a9606bd3b5028275b87b41
vpendo <v.pendo@alustudent.com>
2023-11-06 12:58:57 -0800

    my data

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,6 @@
+ce_in_list(my_list, idx, element):
+        if idx >= 0 and idx < len(my_list):
+            my_list[idx] = element
+            return my_list
+        else:
+            return my_list
