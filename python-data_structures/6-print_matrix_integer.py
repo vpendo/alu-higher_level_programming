06c821b496be6e5096214f3198c655ba9f0a3787
vpendo <v.pendo@alustudent.com>
2023-11-07 05:31:20 -0800

    my data

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-       return
     for row in matrix:
         for col in row:
-            print("{:d}".format(col),end=" ")
+            print("{:d}".format(col), end=" ")
         print()
