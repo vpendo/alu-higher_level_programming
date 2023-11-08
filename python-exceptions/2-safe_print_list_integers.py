3865a5cb20b854c4d939e201a75f1e4ec8a9dc58
vpendo <v.pendo@alustudent.com>
2023-11-08 04:39:39 -0800

    my data

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,9 +3,9 @@ def safe_print_list_integers(my_list=[], x=0):
     start = 0
     for i in range(x):
         try:
-           print("{:d}".format(my_list[i]), end="")
-           start += 1
+            print("{:d}".format(my_list[i]), end="")
+            start += 1
         except (ValueError, TypeError):
-           pass
+            pass
     print()
     return start
