20d10ae764a16b9979efe9145866e3f2817eff14
vpendo <v.pendo@alustudent.com>
2023-11-07 22:43:57 -0800

    my data

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -7,7 +7,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     elif len(tuple_a) > 2:
         tuple_a = tuple_a[:2]
     if len(tuple_b) == 0:
-       tuple_b += (0, 0)
+        tuple_b += (0, 0)
     elif len(tuple_b) == 1:
         tuple_b += (0,)
     elif len(tuple_b) > 2:
