53bdcc413466471e271c249439174c46af005923
vpendo <v.pendo@alustudent.com>
2023-11-11 07:13:05 -0800

    classes

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
-class Square:
-        pass
+# Import the Square class
+Square = __import__('0-square').Square
+# Create a Square object
+my_square = Square()
+# Print the object type
+print(type(my_square))
+# Print the object's attributes
+print(my_square.__dict__)
