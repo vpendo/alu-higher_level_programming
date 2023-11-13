ab7e2f9127b00676a67c0f7e4812733c3879f4bd
vpendo <v.pendo@alustudent.com>
2023-11-13 05:42:24 -0800

    classes

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -3,7 +3,7 @@
 
 
 class Square:
-    """Represent a square."""
+
     def __init__(self, size=0, position=(0, 0)):
         self.size = size
         self.position = position
@@ -11,7 +11,6 @@ class Square:
     @property
     def size(self):
         return (self.__size)
-
 
     @size.setter
     def size(self, value):
