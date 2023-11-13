a0ec14cb18e984aea47f362bce34472d897b655e
vpendo <v.pendo@alustudent.com>
2023-11-13 08:47:20 -0800

    classes

--- python-classes/101-square.py
+++ python-classes/101-square.py
@@ -45,16 +45,15 @@ class Square:
         [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
             [print(" ", end="") for j in range(0, self.__position[0])
-            for k in range(self.__size)
-                print("#", end="")
+            [print("#", end="") for k in range(0, self.__size)]
             print("")
 
     def __str__(self):
-       if self.__size != 0:
-           [print("") for i in range(0, self.__position[1])]
-       for i in range(0, self.__size):
-           [print(" ", end="") for j in range(0, self.__position[0])]
-           [print("#", end="") for k in range(0, self.__size)]
-           if i != self.__size - 1:
-              print("")
-       return ("")
+        if self.__size != 0:
+            [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            if i != self.__size - 1:
+               print("")
+        return ("")
