d2eba3dd41c00386c8825d9397f84a24a1ff71e7
vpendo <v.pendo@alustudent.com>
2023-11-14 15:32:29 +0200

    more classes

--- python-more_classes/101-nqueens.py
+++ python-more_classes/101-nqueens.py
@@ -16,7 +16,7 @@ import sys
 
 
 def init_board(n):
-    """Initialize an `n`x`n` sized chessboard with 0's."""
+    """Initialize an `n`x`n` sized"""
     board = []
     [board.append([]) for i in range(n)]
     [row.append(' ') for i in range(n) for row in board]
