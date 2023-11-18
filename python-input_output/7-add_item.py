1e8fe870129889c8c174a7ae96dc0703f5031ce8
vpendo <v.pendo@alustudent.com>
2023-11-18 21:21:00 +0200

    objects

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,37 +1,20 @@
 #!/usr/bin/python3
-"""Script that adds all arguments to a Python list and saves them to a file."""
+"""Are you docuemnted ?"""
+
 
 import sys
-from typing import List
-from json import dump, load
-
-
-def save_to_json_file(my_obj, filename):
-    """Save an object to a JSON file."""
-    with open(filename, 'w') as f:
-        dump(my_obj, f)
-
-
-def load_from_json_file(filename):
-    """Load an object from a JSON file."""
-    try:
-        with open(filename, 'r') as f:
-            return load(f)
-    except FileNotFoundError:
-        return []
-
 
 if __name__ == "__main__":
-    # Load existing data from the JSON file
-    try:
-        loaded_data = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        loaded_data = []
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file') \
+        .load_from_json_file
 
-    # Add command-line arguments to the data
+    try:
+        loadFile = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        loadFile = []
+
     argc = len(sys.argv)
     for idx in range(1, argc):
-        loaded_data.append(sys.argv[idx])
-
-    # Save the updated data back to the JSON file
-    save_to_json_file(loaded_data, "add_item.json")
+        loadFile.append(sys.argv[idx])
+    save_to_json_file(loadFile, "add_item.json")
