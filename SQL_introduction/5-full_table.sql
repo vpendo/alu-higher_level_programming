67a3fc9573566ee7cf0a17ef7c3edd06caa2df25
vpendo <v.pendo@alustudent.com>
2023-11-30 04:37:56 -0800

    database

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,7 @@
 -- list of all table
 USE hbtn_0c_0;
-SHOW CREATE TABLE first_table;
-SHOW COLUMNS FROM first_table;
+CREATE TABLE `first_table` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(256) NOT NULL,
+  PRIMARY KEY (`id`) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci);
 
