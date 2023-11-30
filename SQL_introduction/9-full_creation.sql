c4ec9b47ff131df67862dadbf2d44b4731236b31
vpendo <v.pendo@alustudent.com>
2023-11-30 05:46:43 -0800

    database

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -4,4 +4,8 @@ CREATE TABLE IF NOT EXISTS second_table (
   name VARCHAR(256) NOT NULL,
   score INT NOT NULL
 );
+INSERT INTO second_table (name, score) VALUES ('John', 10);
+INSERT INTO second_table (name, score) VALUES ('Alex', 3);
+INSERT INTO second_table (name, score) VALUES ('Bob', 14);
+INSERT INTO second_table (name, score) VALUES ('George', 8);
 
