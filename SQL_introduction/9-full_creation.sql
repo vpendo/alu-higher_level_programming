4ca6d66ccacf780e670bb3b05f307ead230f8288
vpendo <v.pendo@alustudent.com>
2023-11-30 05:51:33 -0800

    database

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,11 +1,4 @@
--- list table 
-CREATE TABLE IF NOT EXISTS second_table (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(256) NOT NULL,
-  score INT NOT NULL
-);
-INSERT INTO second_table (name, score) VALUES ('John', 10);
-INSERT INTO second_table (name, score) VALUES ('Alex', 3);
-INSERT INTO second_table (name, score) VALUES ('Bob', 14);
-INSERT INTO second_table (name, score) VALUES ('George', 8);
+-- Create a table second table
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table(id, name, score) VALUES(1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
 
